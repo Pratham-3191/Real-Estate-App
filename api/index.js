f5d403a0a077189e1ecbcccbf9f2ec573c3d037a
Pratham-3191 <168298788+Pratham-3191@users.noreply.github.com>
2024-08-20 19:10:39 +0530

    middleWare for Error

--- api/index.js
+++ api/index.js
@@ -1,12 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
+
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(()=>{
-  console.log("Database is connected") 
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Database is connected")
 })
 const app = express()
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running at port 3000!!")
+});
+
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+   const statusCode=err.statusCode|| 500;
+   const message=err.message || "Server Error"
+   return res.status(statusCode).json({
+    sucess:false,
+    message,
+    statusCode,
+   });
 });
